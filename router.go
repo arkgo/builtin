7b6f9dc998a5464c7896f3fb0cd89420e38b112f
yatniel <yatniel@qq.com>
2020-03-15 13:50:46 +0800

    静态化

--- router.go
+++ router.go
@@ -2,14 +2,13 @@ package builtin
 
 import (
 	"github.com/arkgo/ark"
-	. "github.com/arkgo/base"
 )
 
 func init() {
 
-	ark.Sites.Router("_doc_", Map{
-		"name": "系统文档", "text": "系统文档",
-		"action": func(ctx *ark.Http) {
+	ark.Sites.Register("_doc_", ark.Router{
+		Name: "系统文档", Desc: "系统文档",
+		Action: func(ctx *ark.Http) {
 			ctx.Data["cryptos"] = ark.Cryptos()
 			ctx.Data["results"] = ark.Results(ctx.Lang())
 			ctx.Data["routers"] = ark.Routers(ctx.Site)
