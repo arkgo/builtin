330ffa0bbf17baacd58348ba9df6e29b4cdc5857
yatniel <yatniel@qq.com>
2020-03-18 16:41:04 +0800

    var

--- router.go
+++ router.go
@@ -1,18 +1,39 @@
 package builtin
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/arkgo/ark"
+	. "github.com/arkgo/asset"
+	"github.com/arkgo/asset/util"
 )
 
 func init() {
 
 	ark.Sites.Register("_doc_", ark.Router{
-		Uri:  "/_doc_",
-		Name: "系统文档", Desc: "系统文档",
+		Uri: "/_doc_", Name: "系统文档", Desc: "系统文档",
+		Setting: Map{"passport": true},
 		Action: func(ctx *ark.Http) {
 			ctx.Data["cryptos"] = ark.Cryptos()
 			ctx.Data["results"] = ark.Results(ctx.Lang())
 			ctx.Data["routers"] = ark.Routers(ctx.Site)
+
+			path := "/"
+			if vv, ok := ctx.Args["p"].(string); ok && vv != "" {
+				path = vv
+			}
+
+			client := fmt.Sprintf("1234567890/ios/12.1.0/ios.test.com/1/%d/", time.Now().Unix())
+			sign := util.Md5(client + path)
+			client = client + sign
+
+			ctx.Data["client"] = Map{
+				"client":  client,
+				"example": ark.Encrypt(client),
+				"sign":    sign, "path": path,
+			}
+
 			ctx.View("_doc_")
 		},
 	})
